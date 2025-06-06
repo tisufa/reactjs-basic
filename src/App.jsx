1ec069f128c36ab0ce9b670a171b6ee2ab114fc3
Titus Sutio Fanpula <tisufamoney@gmail.com>
2025-06-07 01:53:28 +0700

    add: using fontawesome with first approach

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,30 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <button
+        style={{
+          background: "green",
+          padding: "0.5rem 1rem",
+          borderRadius: "0.5rem",
+          border: "none",
+          color: "#fff",
+        }}
+      >
+        <em className="fas fa-plus"></em>
+        Add User
+      </button>
+      <button
+        style={{
+          background: "red",
+          padding: "0.5rem 1rem",
+          borderRadius: "0.5rem",
+          border: "none",
+          color: "#fff",
+        }}
+      >
+        <em className="fas fa-trash"></em>
+        Delete User
+      </button>
     </>
   );
 }
